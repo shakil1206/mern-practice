97bb57198330d1086f50b944db07542d8a7c2127
Md Shakil Ahmed <shakilatrai5@gmail.com>
2023-02-08 09:25:17 +0600

    OS and Path module practice

---
+++ practice3/index.js
@@ -0,0 +1,11 @@
+const os = require('os');
+const path = require('path');
+// console.log(os)
+
+// console.log(os.hostname())
+// console.log(os.userInfo())
+// console.log(os.userInfo())
+
+// console.log(path)
+
+// console.log(path.format())
