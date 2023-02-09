92a4771ecf58a28041dc5265e134aa5e35086fcb
Md Shakil Ahmed <shakilatrai5@gmail.com>
2023-02-09 12:50:17 +0600

    static middleware practice

--- practice6/app.js
+++ practice6/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/users.route');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 app.use('/api/user', userRouter);
 
+
+
+app.post('/user', (req, res) => {
+    const name = req.body.name;
+    res.send(`Welcome ${name}`)
+    // res.send()
+})
+
 app.use('/', (req, res) => {
-    res.send("<h1>I am from Home page</h1>")
+    // res.send("<h1>I am from Home page</h1>")
+    res.sendFile(__dirname + '/views/index.html')
+    res.cookie('name', "Shakil Ahmed")
 })
 
 app.use((req, res) => {
--- practice6/routes/users.route.js
+++ practice6/routes/users.route.js
@@ -6,7 +6,17 @@ router.get('/login', (req, res) => {
     res.send("<h1>I am from login page</h1>")
 })
 router.get('/register', (req, res) => {
-    res.send("<h1>I am from register page</h1>")
+
+    res.statusCode = 200;
+    res.sendFile('../'+__dirname + '/views/register.html');
+
+    // res.status(200).json({
+    //     'name': "Shakil Ahmed",
+    //     "message": "I am from register page",
+    //     'status_code': 200
+    // })
+    // res.redirect('/login')
+    // res.send("<h1>I am from register page</h1>")
 })
 
 
