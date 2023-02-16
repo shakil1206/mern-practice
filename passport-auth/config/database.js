2008f22bdf410dc243496558541869df4a3c0b4f
Md Shakil Ahmed <shakilatrai5@gmail.com>
2023-02-16 21:14:23 +0600

    session based authentication by using passport, passport-local strategy and bcrypt

---
+++ passport-auth/config/database.js
@@ -0,0 +1,11 @@
+// require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery',false)
+const dbUrl = process.env.MONGO_URL;
+
+mongoose.connect(dbUrl).then(() => {
+    console.log("DB is connected");
+}).catch((err) => {
+    console.log(err.message);
+    // process.exit(1); 
+})
